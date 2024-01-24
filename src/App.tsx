70739523390e9750b5fa52214353493d5d571993
Tacianjr07 <t.juniior13@gmail.com>
2024-01-24 12:05:48 -0300

    :sparkles:  feat: use styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
+import * as Styles from "./style/style.app";
+
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Our Collection</h1>
-        <p>
-          Introducing our Coffee Collection, a selection of unique coffees from
-          different roast types and origins, expertly roasted in small batches
-          and shipped fresh weekly.
-        </p>
+    <Styles.Container>
+      <Styles.Main>
+        <Styles.Header>
+          <Styles.Title>Our Collection</Styles.Title>
+          <Styles.paragraph>
+            Introducing our Coffee Collection, a selection of unique coffees
+            from different roast types and origins, expertly roasted in small
+            batches and shipped fresh weekly.
+          </Styles.paragraph>
 
-        <div>
-          <button>All Products</button>
-          <button>Available Now</button>
-        </div>
-      </header>
+          <Styles.ButtonsFlex>
+            <button>all products</button>
+            <button>Available now</button>
+          </Styles.ButtonsFlex>
+        </Styles.Header>
 
-      <main>
-        <div>
-          <img
-            src="https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="coffee img"
-          />
-          <div>
-            <p>name coffee</p>
-            <span>price</span>
-          </div>
-          <div>
-            <button></button>
-            <p>votes</p>
-          </div>
-        </div>
-      </main>
-    </div>
+        <Styles.ContainerProducts>
+          <Styles.Products>
+            <img
+              src="https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <div>
+              <p>cafe</p>
+              <span>price</span>
+            </div>
+            <div>
+              <button></button>
+              <p>4.7</p>
+              <span>(65 votes)</span>
+            </div>
+          </Styles.Products>
+        </Styles.ContainerProducts>
+      </Styles.Main>
+    </Styles.Container>
   );
 }
 
